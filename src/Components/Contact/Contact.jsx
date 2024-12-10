b6fc0ab527f1e2dfef0789e825005db408f2f15a
SAI-S12 <sssainath12@gmail.com>
2024-12-10 17:39:13 +0530

    update form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,6 +7,31 @@ import { LiaLinkedin } from 'react-icons/lia'
 import { DiGithub } from 'react-icons/di'
 
 const Contact = ({isOn}) => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "bc9117d7-4dd1-4ed0-ab12-5833e351dc31");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+      event.target.reset()
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className={isOn?"c1":"cc1"}>
       <div className="c2">
@@ -14,21 +39,23 @@ const Contact = ({isOn}) => {
            <h2> Contact me</h2>
            <p><MdEmail/>sxxxxxx12@gmail.com</p>
            <p><BiPhone/>+91 9XXXXXXXX0</p>
-           <p><CiLocationOn/>kamalanagr,chaitanyapuri ,hyderabad </p>
-           <p><LiaLinkedin/>linkedin.com/sai</p>
-           <p><DiGithub/>github/.com/XX/X12</p>
-           <div className="icons">
-
-           </div>
+           <p><CiLocationOn/>Panjagutta,hyderabad </p>
+           <p><LiaLinkedin/>linkedin.com/XYZ</p>
+           <p><DiGithub/>github/.com/XXX/X12</p>
            <button className={isOn?"c5":"c55"}>Download CV</button>
         </div>
         <div className="c4">
-         <div className="c6">
-          <input type="text"  placeholder='Your name'/>
-          <input type="text" placeholder='Your Email' />
-          <textarea name="" id="" placeholder='your message'></textarea>
-          <button className={isOn?"c7":"c77"} > submit</button>
+        <form action="" onSubmit={onSubmit}>
+         
+        <div className="c6">
+        <div className="cc"><h2>Get in Touch</h2></div>
+          <input type="text" name="name" placeholder='Your name'/>
+          <input type="text" name="email" placeholder='Your Email' />
+          <textarea name="message" id="" placeholder='your message'></textarea>
+          <button className={isOn?"c7":"c77"} type='submit'> submit</button>
+          <div className='c12'><p className='c13'>{result}</p></div>
          </div>
+        </form>
         </div>
       </div>
 
