cfa016a08aa7a56da582608ee74af994f178bd0a
SAI-S12 <sssainath12@gmail.com>
2025-11-04 14:47:02 +0530

    updated

--- src/Components/Services/Service.jsx
+++ src/Components/Services/Service.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import "./Service.css"
-import { BsArrowUpCircle } from 'react-icons/bs'
+import React from "react";
+import "./Service.css";
+import { BsArrowUpCircle } from "react-icons/bs";
 import pic1 from "./../../assets/p1.jpg";
 import pic2 from "./../../assets/p2.jpg";
-import pic3 from "./../../assets/p3.jpg";
-import pic4 from "./../../assets/p4.jpg";
-import pic5 from "./../../assets/p5.jpg";
+import pic3 from "./../../assets/banner3.jpg";
+import pic4 from "./../../assets/bannerpic1.jpg";
+import pic5 from "./../../assets/bannerpic2.jpg";
 import pic6 from "./../../assets/p6.jpg";
 
 const Service = ({ isOn }) => {
@@ -15,26 +15,36 @@ const Service = ({ isOn }) => {
         <h2> projects</h2>
         <div className="s4">
           <div className="s5">
-            <img src={pic1} alt="" />
+            <img src={pic5} alt="" />
             <div className="s6">
-              <h2>web E-commerce application</h2>
-              <a href=" https://shopperservice.netlify.app  "><BsArrowUpCircle className='ss2' /></a></div>
+              <h3>web E-commerce application</h3>
+              <a href=" https://shopperservice.netlify.app  ">
+                <a href="">here</a>
+              </a>
+            </div>
           </div>
           <div className="s5">
-            <img src={pic2} alt="" />
+            <img src={pic4} alt="" />
             <div className="s6">
-              <h2>UI/UX design</h2>
-              <BsArrowUpCircle  className='ss2' /></div>
+              <h3>Mobile developement</h3>
+              <p>
+                developeed ecommerce moble application it has 40-50 screens and
+                bottom navigation
+              </p>
+              <a href="">here</a>
+            </div>
           </div>
           <div className="s5">
             <img src={pic3} alt="" />
             <div className="s6">
-              <h2>food web application</h2><a href="https://foodeaterapp.netlify.app "><BsArrowUpCircle  className='ss2' /></a></div>
+              <h3>food web application</h3>
+              <a href="https://foodeaterapp.netlify.app ">here</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -1,26 +1,50 @@
-import React, { useState } from 'react'
-import "./Main.css"
-import logo from "./../../assets/image2-removebg.png"
-import logo1 from "./../../assets/image2.jpg"
-import resume from "../../assets/sairesume12.pdf"
-import { BsGithub, BsLinkedin, BsYoutube } from 'react-icons/bs'
+import React, { useState } from "react";
+import "./Main.css";
+import logo from "./../../assets/image2-removebg.png";
+import logo1 from "./../../assets/image2.jpg";
+import resume from "../../assets/sairesume12.pdf";
+import { BsGithub, BsLinkedin, BsYoutube } from "react-icons/bs";
 
 const Main = ({ setOn, isOn }) => {
-
   return (
     <div className={isOn ? "p11" : "p1"}>
       <div className="p2">
         <div className="p3">
           <div className="p5">
-            <h1 className='p6'>Hello I'm Sai</h1>
-            <h1 className='p7' contentEditable="true">Web Developer</h1>
-            <p className='p8'>seamlessly blending the world of code and creativity as web developer and UI/UX designer , creating captivationg digital experience</p>
+            <h1 className="p6">Hello I'm Sai</h1>
+            <h1 className="p7" contentEditable="true">
+              Web/Mobile Developer
+            </h1>
+            <p className="p8">
+              seamlessly blending the world of code and creativity as web
+              developer and UI/UX designer , creating captivationg digital
+              experience
+            </p>
             <div className={isOn ? "icns" : "icons"}>
-              <a href="https://www.linkedin.com/in/sai-s12/" style={{color:"white"}}> <BsLinkedin /></a>
-              <a href="https://github.com/SAI-S12"> <BsGithub /></a>
-              <a href=""><BsYoutube /></a>
+              <a
+                href="https://www.linkedin.com/in/sai-s12/"
+                style={{ color: "white" }}
+              >
+                {" "}
+                <BsLinkedin />
+              </a>
+              <a href="https://github.com/SAI-S12">
+                {" "}
+                <BsGithub />
+              </a>
+              <a href="">
+                <BsYoutube />
+              </a>
             </div>
-            <button className={isOn ? "p9" : "p99"}><a href={resume} download="resume" style={{color:isOn?'black':"white"}}>Download cv</a></button>
+            <button className={isOn ? "p9" : "p99"}>
+              <a
+                href={resume}
+                download="resume"
+                style={{ color: isOn ? "black" : "white" }}
+              >
+                Download cv
+              </a>
+            </button>
           </div>
         </div>
         <div className="p4">
@@ -30,7 +54,7 @@ const Main = ({ setOn, isOn }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
